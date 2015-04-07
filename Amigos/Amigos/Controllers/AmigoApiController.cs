746bb9c28f33bd48c99f9d375053caf7161fece6
Marta Rodríguez <uo216988@uniovi.es>
2015-04-07 12:50:13 +0200

    Acabado el apartado 3
    Queda sin hacer nada del 4

---
+++ Amigos/Amigos/Controllers/AmigoApiController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Amigos.DataAccessLayed;
+using Amigos.Models;
+
+namespace Amigos.Controllers.API
+{
+    public class AmigoController : ApiController
+    {
+        private AmigoDBContext db = new AmigoDBContext();
+
+        // GET: api/AmigoApi
+        public IQueryable<Amigo> GetAmigos()
+        {
+            return db.Amigos;
+        }
+
+        // GET: api/AmigoApi/5
+        [ResponseType(typeof(Amigo))]
+        public IHttpActionResult GetAmigo(int id)
+        {
+            Amigo amigo = db.Amigos.Find(id);
+            if (amigo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(amigo);
+        }
+
+        // PUT: api/AmigoApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAmigo(int id, Amigo amigo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != amigo.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(amigo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AmigoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/AmigoApi
+        [ResponseType(typeof(Amigo))]
+        public IHttpActionResult PostAmigo(Amigo amigo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Amigos.Add(amigo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = amigo.ID }, amigo);
+        }
+
+        // DELETE: api/AmigoApi/5
+        [ResponseType(typeof(Amigo))]
+        public IHttpActionResult DeleteAmigo(int id)
+        {
+            Amigo amigo = db.Amigos.Find(id);
+            if (amigo == null)
+            {
+                return NotFound();
+            }
+
+            db.Amigos.Remove(amigo);
+            db.SaveChanges();
+
+            return Ok(amigo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AmigoExists(int id)
+        {
+            return db.Amigos.Count(e => e.ID == id) > 0;
+        }
+    }
+}
